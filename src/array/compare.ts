4cf79096f796ffca1538023b31296683b3f7b0fd
PracticeEveryday <developerforus@gmail.com>
2023-04-04 22:44:05 +0900

    :rocket: 배열이 동일한지 비교하는 함수 추가

---
+++ src/array/compare.ts
@@ -0,0 +1,17 @@
+/**
+ * 두 배열의 요소 값들이 일치하면 true 를 반환하고 일치하지 않으면 false를 반환함
+ * @param arr1 비교할 배열1
+ * @param arr2 비교할 배열2
+ * @returns Boolean
+ */
+const equelArray = (arr1: unknown[], arr2: unknown[]): boolean => {
+    if (arr1 === arr2) return true;
+    if (arr1.length !== arr2.length) return false;
+
+    const condition = arr1.every((v) => arr2.find((v2) => v2 === v));
+    if (condition) {
+        return true;
+    }
+
+    return false;
+};
