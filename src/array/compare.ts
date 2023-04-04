d93cbeb570213cc9e42615680e650a6e1c738ad1
PracticeEveryday <developerforus@gmail.com>
2023-04-04 23:00:45 +0900

    :rocket: 순서까지 일치하는 함수 작성

--- src/array/compare.ts
+++ src/array/compare.ts
@@ -15,3 +15,16 @@ export const equelArray = (arr1: unknown[], arr2: unknown[]): boolean => {
 
     return false;
 };
+
+/**
+ * 두 배열의 순서와 요소 값들이 일치하면 true 를 반환하고 일치하지 않으면 false를 반환함
+ * @param arr1 비교할 배열1
+ * @param arr2 비교할 배열2
+ * @returns Boolean
+ */
+
+export const stricEqualArray = (arr1: unknown[], arr2: unknown[]): boolean => {
+    if (arr1.length !== arr2.length) return false;
+
+    return arr1.every((v, idx) => v === arr2[idx]);
+};
